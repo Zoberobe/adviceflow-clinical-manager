70bc4447b2916c58b35ecb4fefe3ee6c8ffb9257
heyma <heyma@DESKTOP-FGQ4HK0>
2026-05-25 19:25:31 -0300

    chore: inicializar estrutura do front-end em react com vite e dockerfile

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      fontFamily: 'sans-serif',
+      backgroundColor: '#f0f2f5' 
+    }}>
+      <h1>AdviceFlow Front-end Operacional</h1>
+    </div>
+  </React.StrictMode>,
+)
